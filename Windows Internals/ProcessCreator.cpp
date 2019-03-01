d0575537d45bac62459a8b3ff72e5dfb5a0a8826
akhil-yeluri <akhilyeluri6@gmail.com>
2019-03-01 10:25:09 -0800

    creating child process

--- Windows Internals/ProcessCreator.cpp
+++ Windows Internals/ProcessCreator.cpp
@@ -1,8 +1,33 @@
 #include<iostream>
 #include<Windows.h>
+#include<tchar.h>
 using namespace std;
-int main()
+int _tmain(int argc,WCHAR * argv[],WCHAR * env[])
 {
+	int n;
 	BOOL processid;
-	processid = CreateProcess(TEXT("calc.exe"),NULL,NULL,FALSE,NORMAL_PRIORITY_CLASS,NULL,NULL);
+	STARTUPINFO  startupinfo;
+	PROCESS_INFORMATION processinfo;
+	ZeroMemory(&startupinfo,sizeof(startupinfo));
+	ZeroMemory(&processinfo, sizeof(processinfo));
+	startupinfo.cb = sizeof(startupinfo);
+	//WCHAR var[10] = "calc.exe";
+	cout << "Enter your choice\n1.Calculator\n2.Notepad";
+	cin >> n;
+	switch (n)
+	{
+	case 1:	processid = CreateProcess(NULL, argv[1], NULL, NULL, FALSE, NORMAL_PRIORITY_CLASS, NULL, NULL, &startupinfo, &processinfo);
+		    cout << "ProcessId:" << processinfo.dwProcessId << endl;
+		    cout << "Handle:" << processinfo.hProcess << endl;
+		    break;
+
+	case 2:	processid = CreateProcess(NULL, argv[2], NULL, NULL, FALSE, NORMAL_PRIORITY_CLASS, NULL, NULL, &startupinfo, &processinfo);
+		    cout <<"ProcessId:"<<processinfo.dwProcessId << endl;
+			cout << "Handle:" << processinfo.hProcess << endl;
+		    break;
+	default:return 0;
+	}
+	//cout << processid<<endl<<GetLastError();
+	cin >> n;
+	return 0;
 }
