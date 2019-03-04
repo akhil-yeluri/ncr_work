b11306145f2d6ab9d044fad90c5b5abb2992345e
akhil-yeluri <akhilyeluri6@gmail.com>
2019-03-04 15:02:19 -0800

    using critical section functions

---
+++ Windows Internals/CriticalSectionFunctions.cpp
@@ -0,0 +1,48 @@
+using namespace std;
+#include<Windows.h>
+#include<iostream>
+#define limit 100
+
+long GlobalIncrementVariable;
+int traversal;
+
+CRITICAL_SECTION cs;
+
+DWORD WINAPI Function1(LPVOID lpparameter)
+{
+	EnterCriticalSection(&cs);
+	GlobalIncrementVariable++;
+	LeaveCriticalSection(&cs);
+	return 0;
+}
+
+DWORD WINAPI Function2(LPVOID LAP)
+{
+	EnterCriticalSection(&cs);
+	cout << "HEllo world"<<endl;
+	LeaveCriticalSection(&cs);
+	return 0;
+}
+
+int main()
+{
+	HANDLE ThreadHandler[limit];
+	//InitializeCriticalSection(&cs);
+	InitializeCriticalSectionAndSpinCount(&cs,100);
+	while ((traversal++)<=limit) {
+		GlobalIncrementVariable = 0;
+		ThreadHandler[0] = CreateThread(NULL, 0, &Function1, 0, 0, NULL);
+		ThreadHandler[1]=CreateThread(NULL, 0, &Function2, 0, 0, NULL);
+		WaitForMultipleObjects(2,ThreadHandler,1,INFINITE);
+		cout << "Value of variable:" << GlobalIncrementVariable<<endl;
+		if (GlobalIncrementVariable ==0)
+		{
+			cout << "Threads are Inconsistent\n";
+			getchar();
+		}
+	}
+
+	cout << "Shared Integer Among Threads is Consistent\n";
+	getchar();
+	return 0;
+}
