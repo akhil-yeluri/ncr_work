e3a95a98dce63e34d543f3c038324b7df58e3d71
akhil-yeluri <akhilyeluri6@gmail.com>
2019-03-06 22:53:22 +0530

    short hand notation modified

--- cpp/Expansion.cpp
+++ cpp/Expansion.cpp
@@ -1,13 +1,19 @@
 using namespace std;
 #include<iostream>
 #include<string>
-void print(char a,char b)
+#include<string.h>
+int print(char a,char b)
 {
-	
+	if (a > b)
+	{
+		cout << "Invalid expression";
+		return 0;
+	}
 	while (a <= b) {
 		cout << a;
 		a++;
 	}
+	return 1;
 }
 
 int main()
@@ -15,14 +21,30 @@ int main()
 	string input;
 	cout << "enter an expression\n";
 	getline(cin, input);
-	int length = 0;
-	while (input[length] != '\0')
-		length++;
-	cout << length;
+	int length = input.length();
+
+	if (input[0] == '-' || input[length - 1] == '-')
+	{
+		cout <<"Invalid Expression\nPress any key to exit";
+		getchar();
+		return 0;
+	}
+
+	for(int i=2;i<length;i++)
+		if(input[i]=='-' && (input[i-2]=='-' || input[i-1]=='-'))
+		{
+			cout << "Invalid Expression\nPress any key to exit";
+			getchar();
+			return 0;
+		}
+	
+
 	for (int i = 0; i<length; i++)
 		if (input[i] == '-' && input[i + 1] != '\0')
 		{
-			print(input[i - 1], input[i+ 1]);
+			if(!print(input[i - 1], input[i+ 1]))
+				return 0;
+			
 			i++;
 		}
 		else
