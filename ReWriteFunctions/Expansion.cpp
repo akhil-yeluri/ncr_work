3e724aa910294f5cf0541e5dea267726606ab84c
akhil-yeluri <akhilyeluri6@gmail.com>
2019-03-20 08:45:34 +0530

    added commits for rewrite functions

--- ReWriteFunctions/Expansion.cpp
+++ ReWriteFunctions/Expansion.cpp
@@ -2,14 +2,18 @@ using namespace std;
 #include<iostream>
 #include<string>
 #include<string.h>
+
+//function for printing the expansion sequence
 int print(char a,char b)
 {
+	//conditon: breaks where expansion interval is invalid
 	if (a > b)
 	{
-		cout << "Invalid expression\nPress any key to exit";
+		cout << "\nThe Sequence encountered a Invalid expression and stopped execution\nPress any key to exit";
 		getchar();
 		return 0;
 	}
+	//prints sequence if expansion interval is valid
 	while (a <= b) {
 		cout << a;
 		a++;
@@ -24,7 +28,7 @@ int main()
 	cout << "enter an expression\n";
 	getline(cin, input);
 	int length = input.length();
-
+	//condition for checking for valid expression at start and end of string
 	if (input[0] == '-' || input[length - 1] == '-')
 	{
 		cout <<"Invalid Expression\nPress any key to exit";
@@ -32,6 +36,7 @@ int main()
 		return 0;
 	}
 
+	//condition for checking for proper spacing of '-' operator
 	for(i=2;i<length;i++)
 		if(input[i]=='-' && (input[i-2]=='-' || input[i-1]=='-'))
 		{
@@ -42,6 +47,7 @@ int main()
 	
 
 	for ( i = 0; i<length-1; i++)
+		//function call if the expression is valid
 		if (input[i] == '-' && input[i + 1] != '\0')
 		{
 			if(!print(input[i - 1], input[i+ 1]))
--- ReWriteFunctions/StringFunctions.cpp
+++ ReWriteFunctions/StringFunctions.cpp
@@ -7,47 +7,63 @@
 using namespace std;
 int input;
 
+
+//function for string comparision
 int strcmp(string s1,string s2)
 {
 
 	int length = 0;
 	while (length < s1.length() && length < s2.length())
 	{
+	//if string 1 is lexicographically greater than string2
 		if (s1[length] < s2[length])
 			return -1;
-		else
+		//if string 2 is lexicographically greater than string1
+		else  
 			if (s1[length] > s2[length])
 				return 1;
 			else
 				length++;
 	}
+	//if source and destination string lengths are not equal
 	if (s1.length() != s2.length())
 		return 1;
+	//if given strings are similar
 	return 0;
 }
 
+
+
+//function for copying contents of one string into another
 string strcpy(string s1,string s2)
 {
 		s2 = s1;
 		return s2;
 }
 
+
+//function for string concatenating
 char* strcat(char *s1,char* s2)
 {
 	int i = 0;
+	//traversing until end of destination string
 	for (i = 0; s1[i] != '\0'; i++)
 		continue;
+	//concatenating contents of source string to destination string
 	for (int j = 0; s2[j] != '\0'; j++)
 		s1[i++] = s2[j];
 	s1[i] = '\0';
-	realloc(s1, strlen(s1) + strlen(s2));
+	//realloc(s1, strlen(s1) + strlen(s2));
 	return s1;
 }
 
+
+//function for reversal of string
 string strrev(string s)
 {
 	int length = s.length()-1;
 	int i = 0;
+	//swapping operation
 	while (i <= s.length()/2-1)
 	{
 		char c = s[i];
@@ -65,6 +81,7 @@ int main()
 	while (1) {
 		cout << "Enter your choice\n1.check string equality\n2.Reverse a String\n3.String Copy\n4.String Concatenate\n";
 		cin >> input;
+        //checking string equality
 		if (input == 1)
 		{
 			string s1, s2;
@@ -80,7 +97,7 @@ int main()
 			cout << "\n";
 			continue;
 		}
-
+        //reverse a string
 		if (input == 2)
 		{
 			string s;
@@ -91,6 +108,8 @@ int main()
 			cout << "\n";
 			continue;
 		}
+		
+		//string copy
 		if (input == 3)
 		{
 			string s1, s2;
@@ -103,7 +122,7 @@ int main()
 			cout << "\n";
 			continue;
 		}
-
+		//string concatenation
 		if (input == 4)
 		{
 			char *s1 = new char[size];
@@ -121,7 +140,7 @@ int main()
 			delete s1, s2;
 			continue;
 		}
-
+        //invalid option case
 		else
 		{
 			cout << "Invalid Option\nPress any key to Exit\n";
